2f0d5e54a1a7a713e482c600a92db0719273d3ec
uniguacu-developer <developer@uniguacu.com.br>
2024-06-05 09:12:14 -0300

    criado um botão para add tarefas

--- src/app/app/(home)/page.tsx
+++ src/app/app/(home)/page.tsx
@@ -1,16 +1,28 @@
 import {
   DashboardPage,
   DashboardPageHeader,
+  DashboardPageHeaderNav,
   DashboardPageHeaderTitle,
   DashboardPageMain,
 } from '@/components/dashboard/dashboard-page'
 import { TodoDataTable } from './_components/todo-data-table'
+import { TodoUpsertSheet } from './_components/todo-upsert-sheet'
+import { Button } from '@/components/ui/button'
+import { PlusIcon } from '@radix-ui/react-icons'
 
 export default async function Page() {
   return (
     <DashboardPage>
       <DashboardPageHeader>
         <DashboardPageHeaderTitle>Tarefas</DashboardPageHeaderTitle>
+        <DashboardPageHeaderNav>
+          <TodoUpsertSheet>
+            <Button variant="outline" size="sm">
+              <PlusIcon className="w-4 h-4 mr-2" />
+              Adicionar tarefa
+            </Button>
+          </TodoUpsertSheet>
+        </DashboardPageHeaderNav>
       </DashboardPageHeader>
       <DashboardPageMain>
         <TodoDataTable />
